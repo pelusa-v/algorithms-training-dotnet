843ff9edcbe602922688129ca9ed97079a03ea9c
pelusa-v <razor0164@gmail.com>
2024-02-27 01:03:01 -0500

    Insertion Sort first version Done

---
+++ algorithms-training-dotnet.console/Program.cs
@@ -0,0 +1,8 @@
+﻿using algorithms_training_dotnet.console.Sortings;
+using algorithms_training_dotnet.console.Utils;
+
+var testArray = new int[] { 9, 8, 7, 4, 1, 6, 2 };
+
+ArrayUtils.ShowArrayContent(testArray);
+InsertionSort.Sort(testArray);
+ArrayUtils.ShowArrayContent(testArray);
---
+++ algorithms-training-dotnet.console/Utils/ArrayUtils.cs
@@ -0,0 +1,14 @@
+﻿namespace algorithms_training_dotnet.console.Utils;
+
+public class ArrayUtils
+{
+    public static void ShowArrayContent(int[] array)
+    {
+        Console.Write("[");
+        foreach (var item in array)
+        {
+            Console.Write($"{item}, ");
+        }
+        Console.Write("]\n");
+    }
+}
